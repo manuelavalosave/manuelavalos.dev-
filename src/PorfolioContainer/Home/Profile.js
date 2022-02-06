425a90982ba7fd68283a0b5f9bb732b90934f639
Manuel Avalos <manuelavalosave@gmail.com>
2022-02-06 13:09:41 -0600

    listo

---
+++ src/PorfolioContainer/Home/Profile.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Profile() {
+  return (
+  <div className='profile-container'>
+      <div className='profile-parent'>
+          <div className='profile-datails'>
+              <div className='colz'>
+                  <a href='https://www.instagram.com/avalos_ia/'>
+                      <li className='fa fa-instagram'></li>
+                  </a>
+                
+              </div>
+          </div>
+      </div>
+
+  </div>
+  );
+}
